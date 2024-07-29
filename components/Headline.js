32d01e3124cccae88a177f3c199c03c040df29b9
kifumi01 <aki.suzu.1126@gmail.com>
2024-07-29 09:09:41 +0900

    props

---
+++ components/Headline.js
@@ -0,0 +1,36 @@
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import Image from "next/image";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Headline(props) {
+  return (
+    <div>
+        <div className={styles.description}>
+          <p>
+            {props.title}
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+
+    </div>
+  );
+}
